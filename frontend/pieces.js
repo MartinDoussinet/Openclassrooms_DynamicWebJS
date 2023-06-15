f7ac9be4a5f13a1214bb8f0f0228c4603a6e0734
Martin Doussinet <martin.doussinet@hei.yncrea.fr>
2023-06-15 17:39:09 +0200

    End of Part2 : Adding a function to refresh our site and adding a range input to select a max price for displayed items

--- frontend/pieces.js
+++ frontend/pieces.js
@@ -2,61 +2,71 @@
     Récupération des pièces depuis le fichier JSON
     ______________________________________________  */
 
-const reponse = await fetch("pieces-autos.json");
-const pieces = await reponse.json()
+const pieces = await fetch("pieces-autos.json").then(pieces => pieces.json());
 
-for (let i = 0; i < pieces.length; i++) {
+/*  _______________________________________________________________________________________________________________
+    On créé une fonction pour générer les pièces dans la section "fiches" afin d'actualiser la page plus facilement
     
-    /*  ________________________________
-        Création des différentes balises
-        ________________________________    */
+    On génère ensuite la page une première fois
+    _______________________________________________________________________________________________________________  */
+
+function generationPieces(pieces) {
     
-    const article = pieces[i];
+    for (let i = 0; i < pieces.length; i++) {
 
-    const imageElement = document.createElement("img");
-    imageElement.src = article.image;
+        /*  ________________________________
+            Création des différentes balises
+            ________________________________  */
+        
+        const article = pieces[i];
 
-    const nomElement = document.createElement("h2");
-    nomElement.innerText = article.nom;
+        const imageElement = document.createElement("img");
+        imageElement.src = article.image;
 
-    const prixElement = document.createElement("p");
-    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+        const nomElement = document.createElement("h2");
+        nomElement.innerText = article.nom;
 
-    const categorieElement = document.createElement("p");
-    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+        const prixElement = document.createElement("p");
+        prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 
-    //__________________________________________________________________________________________
-    //Exercice P1C4 - Ajout de la description et du stock
+        const categorieElement = document.createElement("p");
+        categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 
-    const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+        //__________________________________________________________________________________________
+        //Exercice P1C4 - Ajout de la description et du stock
 
-    const stockElement = document.createElement("p");
-    stockElement.innerText = article.disponibilité ? "En stock" : "Rupture de stock";
-    //__________________________________________________________________________________________
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
 
-    /*  ______________________________
-        On rattache les balises au DOM
-        ______________________________  */
+        const stockElement = document.createElement("p");
+        stockElement.innerText = article.disponibilité ? "En stock" : "Rupture de stock";
+        //__________________________________________________________________________________________
 
-    const sectionFiches = document.querySelector(".fiches");
+        /*  _________________________________________________
+            On rattache les balises au DOM (section "fiches")
+            _________________________________________________  */
 
-    const pieceElement = document.createElement("article");
-    sectionFiches.appendChild(pieceElement)
+        const sectionFiches = document.querySelector(".fiches");
 
-    pieceElement.appendChild(imageElement);
-    pieceElement.appendChild(nomElement);
-    pieceElement.appendChild(prixElement);
-    pieceElement.appendChild(categorieElement);
+        const pieceElement = document.createElement("article");
+        sectionFiches.appendChild(pieceElement)
 
-    //____________________________________________________
-    //Exercice P1C4 - Ajout de la description et du stock
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(nomElement);
+        pieceElement.appendChild(prixElement);
+        pieceElement.appendChild(categorieElement);
 
-    pieceElement.appendChild(descriptionElement);
-    pieceElement.appendChild(stockElement);
+        //____________________________________________________
+        //Exercice P1C4 - Ajout de la description et du stock
 
-    //____________________________________________________
+        pieceElement.appendChild(descriptionElement);
+        pieceElement.appendChild(stockElement);
+
+        //____________________________________________________
+    }
 }
+
+generationPieces(pieces)
 
 /*  __________________________________
     Création des boutons de tri/filtre
@@ -68,18 +78,18 @@ btnTrierCroissant.addEventListener("click", () => {
     piecesOrdonees.sort(function (a,b) {
         return a.prix - b.prix;
     });
-    console.log(pieces);
-    console.log(piecesOrdonees);
+    document.querySelector(".fiches").innerHTML = "";
+    generationPieces(piecesOrdonees);
 });
 
 const btnFiltrerPrix = document.querySelector(".btn-filtrer-prix");
 btnFiltrerPrix.addEventListener("click", () => {
     const piecesFiltrees = pieces.filter(piece => piece.prix < 35);
-    console.log(pieces);
-    console.log(piecesFiltrees);
+    document.querySelector(".fiches").innerHTML = "";
+    generationPieces(piecesFiltrees);
 });
 
-//____________________________________________________________________________________
+//_____________________________________________________________________________________
 //Exercice P2C1 - Ajouter le tri décroissant et le filtre des pièces sans description
 
 const btnTrierDecroissant = document.querySelector(".btn-trier-decroissant");
@@ -88,23 +98,36 @@ btnTrierDecroissant.addEventListener("click", () => {
     piecesOrdonees.sort(function (a,b) {
         return b.prix - a.prix;
     });
-    console.log(pieces);
-    console.log(piecesOrdonees);
+    document.querySelector(".fiches").innerHTML = "";
+    generationPieces(piecesOrdonees);
 });
 
 const btnFiltrerDescription = document.querySelector(".btn-filtrer-description");
 btnFiltrerDescription.addEventListener("click", () => {
     const piecesFiltrees = pieces.filter(piece => piece.description != undefined);
-    console.log(pieces);
-    console.log(piecesFiltrees);
+    document.querySelector(".fiches").innerHTML = "";
+    generationPieces(piecesFiltrees);
 });
 
-//____________________________________________________________________________________
+//_____________________________________________________________________________________
+
+//_____________________________________________________________________________________
+//Exercice P2C3 - Ajouter un input range pour définir le prix max des pièces à afficher
+
+const rangePrixMax = document.querySelector(".prixMax");
+rangePrixMax.addEventListener("change", () => {
+    console.log(rangePrixMax.value)
+    const piecesFiltrees = pieces.filter(piece => piece.prix <= rangePrixMax.value);
+    document.querySelector(".fiches").innerHTML = "";
+    generationPieces(piecesFiltrees);
+})
+
+//_____________________________________________________________________________________
 
 
 /*  _______________________________________
     Remplissage de la section 'fiches'
-    _______________________________________    */
+    _______________________________________  */
 
 const nomsAbordables = pieces.map(piece => piece.nom);
 for (let i = pieces.length - 1; i >= 0; i--) {
@@ -122,7 +145,7 @@ for (let i = 0; i < nomsAbordables.length; i++) {
 
 document.querySelector(".abordables").appendChild(abordablesElements);
 
-//____________________________________________________________________
+//___________________________________________________________________________
 //Exercice P2C2 - Ajout des pièces disponibles avec leur prix
 
 const nomsDisponibles = pieces.map(piece => piece.nom);
@@ -143,4 +166,4 @@ for (let i = 0; i < nomsDisponibles.length; i++) {
 
 document.querySelector(".disponibles").appendChild(disponiblesElements);
 
-//____________________________________________________________________
+//___________________________________________________________________________
